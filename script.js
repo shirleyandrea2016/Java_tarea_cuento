40adc12946219bb34c0e0b8931060ac0baa62a0b
shirleyandrea2016 <smirabap@gmail.com>
2016-03-27 09:18:17 -0500

    CALCULADORA DEL TIEMPO Y CALCULAR LA EDAD Y CASTING parseInt()

--- script.js
+++ script.js
@@ -10,7 +10,9 @@ document.write ("TA-DA!!! Su clave es:" +clave.toUpperCase());
 document.write ("El valor original de la clave es:" +clave);
 alert ("gracias por visitar")
 console.log ("fin de programa");*/
-alert("Sigue las instrucciones.");
+
+/*CUENTO EN JAVA*/
+/*alert("Sigue las instrucciones.");
 var sustantivo1 = prompt("Ingrese un Sustantivo: ");
 var adjectivo1 = prompt("Ingrese un Adjectivo: ");
 var verbo1 = prompt("Ingrese un verbo: ");
@@ -19,4 +21,26 @@ document.write("<h1></h1><p>" + cuento + "</p>");
 var cuento = "El " + adjectivo1 + " " + sustantivo1 + " queria " + verbo1 + " en la noche oscura y fría";
 document.write("<h1></h1><p>" + cuento + "</p>");
 var cuento = "El " + adjectivo1 + " " + sustantivo1 + " queria " + verbo1 + " junto a la familia huesitos!";
-document.write("<h1></h1><p>" + cuento + "</p>");
+document.write("<h1></h1><p>" + cuento + "</p>");*/
+
+/*CALCULADORA DEL TIEMPO*/
+/*var segundosPorMinuto = 60;
+var minutosPorHora = 60;
+var horasPorDia = 24;
+var segundosPorDia = segundosPorMinuto * minutosPorHora * horasPorDia;
+document.write("<h1>Calculadora del Tiempo</h1><p>Hay " + segundosPorDia	+ " en un dia</p>");
+*/
+
+/*CALCULAR LA EDAD*/
+/*var anios=prompt("Ingrese su edad");
+var horasPorDia = 24;
+var anVividos=anios*horasPorDia;
+document.write ("<h1>Calculadora del Tiempo</h1><p>Has vivido " + anVividos + " dias aproximadamente</p>");
+*/
+
+/*CASTING parseInt()*/
+var peliculasVistas = prompt("Cuantas peliculas has visto este mes?");
+var seriesVistas = prompt("Cuantas series has visto este mes?");
+var tiempoPantalla = parseInt(peliculasVistas) + parseInt(seriesVistas);
+document.write ('Wow! Has visto ' + tiempoPantalla + " veces minimo la pantalla.");
+ console.log ("Programa completado" )
