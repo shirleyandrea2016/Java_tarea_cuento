ad52cc82d551dc2be98194512113145430f999b8
shirleyandrea2016 <smirabap@gmail.com>
2016-03-27 08:32:29 -0500

    Tarea programación. Cuento infantil, utilización de variables

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log ("Inicio de programa");
+
+alert ("Hola Javascript");
+/*document.write ("<h1>saludo inicial</h1>");
+var nombre = prompt ("Cual es tu equipo de futbol favorito");
+document.write ("El equipo es:" +nombre);
+var clave = prompt ("Hola usuario ingrese su clave");
+document.write ("La longitud de su clave es:" +clave.length);
+document.write ("TA-DA!!! Su clave es:" +clave.toUpperCase());
+document.write ("El valor original de la clave es:" +clave);
+alert ("gracias por visitar")
+console.log ("fin de programa");*/
+alert("Sigue las instrucciones.");
+var sustantivo1 = prompt("Ingrese un Sustantivo: ");
+var adjectivo1 = prompt("Ingrese un Adjectivo: ");
+var verbo1 = prompt("Ingrese un verbo: ");
+var cuento = "El " + adjectivo1 + " " + sustantivo1 + " queria " + verbo1 + " en el frondoso bosque tenebroso!";
+document.write("<h1></h1><p>" + cuento + "</p>");
+var cuento = "El " + adjectivo1 + " " + sustantivo1 + " queria " + verbo1 + " en la noche oscura y fría";
+document.write("<h1></h1><p>" + cuento + "</p>");
+var cuento = "El " + adjectivo1 + " " + sustantivo1 + " queria " + verbo1 + " junto a la familia huesitos!";
+document.write("<h1></h1><p>" + cuento + "</p>");
